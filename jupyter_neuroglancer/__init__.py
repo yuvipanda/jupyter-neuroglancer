19cb2963ca7fc0f3910e62727645ade1f6922187
YuviPanda <yuvipanda@gmail.com>
2024-07-02 16:33:57 -0700

    Cleanup API

--- jupyter_neuroglancer/__init__.py
+++ jupyter_neuroglancer/__init__.py
@@ -5,6 +5,7 @@ class SidecarViewer:
     def __init__(self, viewer):
         self.viewer = viewer
         self.sidecar = Sidecar()
+        self.is_visible = False
 
     def __enter__(self):
         return self.sidecar.__enter__()
@@ -12,11 +13,16 @@ class SidecarViewer:
     def __exit__(self):
         return self.sidecar.__exit__()
 
-    def open(self):
-        frame = IFrame(self.viewer.get_viewer_url(), width='100%', height='100%')
+    def show(self):
+        if not self.is_visible:
+            frame = IFrame(self.viewer.get_viewer_url(), width='100%', height='100%')
 
-        self.sidecar.__enter__()
-        display(frame)
+            self.sidecar.__enter__()
+            display(frame)
 
-    def close(self):
-        self.sidecar.__exit__()
+            self.is_visible = True
+
+    def hide(self):
+        if self.is_visible:
+            self.sidecar.__exit__()
+            self.is_visible = False
